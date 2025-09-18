bf2b5de7354b386b9d11f899ef8aa3f18f2faaaa
giann589 <130203108+giann589@users.noreply.github.com>
2025-09-18 13:53:41 -0700

    UI Stuff
    - Added Main Menu
    - Added Options Menu
    - Added Credits Menu

---
+++ main_menu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level.tscn")
+
+func _on_options_pressed() -> void:
+	get_tree().change_scene_to_file("res://options.tscn")
+	
+func _on_credits_pressed() -> void:
+	get_tree().change_scene_to_file("res://credits.tscn")
