ed2ada8083dc6a2e9a378b161a985c7aec13ba0e
Saber <130203108+giann589@users.noreply.github.com>
2025-09-23 22:22:24 -0700

    Added some brains to enemy
    - Only shoots when not behind wall

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,14 +1,15 @@
 extends CharacterBody2D
 
 var shoot_cooldown = 2.0
+var health = 100
+var speed = 100
 
-@export var health = 100
-@export var speed = 100
 @export var bullet_scene: PackedScene
 
 @onready var shoot_timer = $Shoot_Timer
 @onready var shoot_pointer = $Bullet_Spawner
 @onready var player: Node2D = get_tree().get_first_node_in_group("player")
+@onready var ray: RayCast2D = $RayCast2D
 
 
 func _ready():
@@ -17,11 +18,11 @@ func _ready():
 
 
 func _process(delta):
-	if player:
+	if player and can_see_player():
 		look_at(player.global_position)
 
 func _on_shoot_timer_timeout():
-	if player:
+	if player and can_see_player():
 		shoot()
 		
 func shoot():
@@ -30,6 +31,18 @@ func shoot():
 	
 	bullet.global_position = shoot_pointer.global_position
 	bullet.direction = (player.global_position - bullet.global_position).normalized()
+	
+func can_see_player() -> bool:
+	if not player:
+		return false
+	ray.target_position = to_local(player.global_position)
+	ray.force_raycast_update()
+	
+	if ray.is_colliding():
+		var collider = ray.get_collider()
+		if collider.is_in_group("player"):
+			return true
+	return false
 	
 
 func take_damage(amount) -> void:
