4e6e6770fdfa58a26e9e3eda7ef219cd2a6f25c3
Saber <130203108+giann589@users.noreply.github.com>
2025-09-24 22:14:29 -0700

    Added Lighting

--- main_menu.gd
+++ main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/level.tscn")
+	get_tree().change_scene_to_file("res://map.tscn")
 
 func _on_options_pressed() -> void:
 	get_tree().change_scene_to_file("res://options.tscn")
